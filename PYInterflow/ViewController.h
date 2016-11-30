1d4e63dfbc0adf7315d59e55203af2a12c646cae
飘逸A186 <wlpiaoyi@piaoyia186.local>
2016-11-30 15:58:40 +0800

    Initial Commit

---
+++ PYInterflow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PYInterflow
+//
+//  Created by wlpiaoyi on 2016/11/30.
+//  Copyright © 2016年 wlpiaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PYInterflow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PYInterflow
+//
+//  Created by wlpiaoyi on 2016/11/30.
+//  Copyright © 2016年 wlpiaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
